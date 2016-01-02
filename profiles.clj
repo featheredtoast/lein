8dd34da0619241fe12c458fb72c8ea4e985ff94b
Jeff Wong <awole20@gmail.com>
2016-01-02 14:06:40 -0800

    repl correct deps

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,9 @@
 {:user
   {   
    :mirrors {#"clojars" {:name "clojars mirror"
-                         :url "https://clojars-mirror.tcrawley.org/repo/"}}}
+                         :url "https://clojars-mirror.tcrawley.org/repo/"}}
+   :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+   :plugins [[cider/cider-nrepl "0.11.0-snapshot"]]}
  
  :repl
- {:plugins [[cider/cider-nrepl "0.11.0-snapshot"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+ {:plugins [[cider/cider-nrepl "0.11.0-snapshot"]]}}
