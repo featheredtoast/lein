aa9fc9f8351999d3e47868b759c7c68bc774e044
Jeff Wong <awole20@gmail.com>
2016-06-22 15:37:22 -0700

    some attempt at getting linting in clojure experience

--- profiles.clj
+++ profiles.clj
@@ -6,5 +6,8 @@
  
  :repl
  {
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]]}}
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                 [acyclic/squiggly-clojure "0.1.5"]]
+  :plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]]
+  :env {:squiggly {:checkers [:eastwood :typed]
+                   :eastwood-exclude-linters [:unlimited-use]}}}}
