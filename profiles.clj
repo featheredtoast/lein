53e0adc7a2fb6666fcf7c2d606cb028b92f0c6b5
Jeff Wong <awole20@gmail.com>
2016-06-22 10:22:01 -0700

    update cider

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,10 @@
 {:user
   {   
-   :mirrors {#"clojars" {:name "clojars mirror"
-                         :url "https://clojars-mirror.tcrawley.org/repo/"}}
+   
    :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-   :plugins [[cider/cider-nrepl "0.11.0-snapshot"]]}
+   :plugins [[cider/cider-nrepl "0.12.0"]]}
  
  :repl
- {:plugins [[cider/cider-nrepl "0.11.0-snapshot"]]}}
+ {
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+  :plugins [[cider/cider-nrepl "0.12.0"]]}}
