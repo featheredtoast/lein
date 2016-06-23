36eba792975c16311439a4b1519bcd6c42136672
Jeff Wong <awole20@gmail.com>
2016-06-23 10:45:39 -0700

    remove squigglies

--- profiles.clj
+++ profiles.clj
@@ -6,8 +6,5 @@
  
  :repl
  {
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                 [acyclic/squiggly-clojure "0.1.5"]]
-  :plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]]
-  :env {:squiggly {:checkers [:eastwood :typed :kibit]
-                   :eastwood-exclude-linters [:unlimited-use]}}}}
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+  :plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]]}}
