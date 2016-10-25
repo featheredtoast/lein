02960cd4ce99788faad6d7def8cb0d8a0a7ff8a5
Jeff Wong <awole20@gmail.com>
2016-10-24 17:28:15 -0700

    repl bump

--- profiles.clj
+++ profiles.clj
@@ -2,9 +2,9 @@
   {   
    
    :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-   :plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]]}
+   :plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]]}
  
  :repl
  {
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-  :plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]]}}
+  :plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]]}}
