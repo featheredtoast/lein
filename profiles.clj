755d85513aeccea9e401c5c9cf26a150c30bf47b
Jeff Wong <awole20@gmail.com>
2017-01-02 01:04:10 -0800

    ancient and slamhount

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,10 @@
 {:user
   {   
-   
-   :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-   :plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]]}
+   :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                  [slamhound "1.5.5"]]
+   :plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
+             [lein-ancient "0.6.10"]]
+   :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
  
  :repl
  {
